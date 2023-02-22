3df1a87500807dfb9b765ef5eb4b22102156860a
Deran Decker <me@derandecker.com>
2023-02-22 05:31:54 -1000

    initial bottom nav bar setup

--- app/src/main/java/com/derandecker/fbmessengeruiclone/ui/MainScreen.kt
+++ app/src/main/java/com/derandecker/fbmessengeruiclone/ui/MainScreen.kt
@@ -1,7 +1,16 @@
 package com.derandecker.fbmessengeruiclone.ui
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import com.derandecker.fbmessengeruiclone.ui.theme.FBMessengerUICloneTheme
 
@@ -13,7 +22,34 @@ fun DefaultPreview() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
-    Text(text = "Hello There!")
+    Scaffold(
+        bottomBar = {
+            androidx.compose.material3.NavigationBar(
+                containerColor = MaterialTheme.colorScheme.background,
+            ) {
+                NavigationBarItem(
+                    selected = true,
+                    onClick = { },
+                    label = {
+                        Text(
+                            text = "Home",
+                            fontWeight = FontWeight.SemiBold,
+                        )
+                    },
+                    icon = {
+                        Icon(imageVector = Icons.Rounded.Home, contentDescription = "")
+                    }
+                )
+            }
+        },
+        content = {
+            PaddingValues(
+                top = it.calculateTopPadding()
+            )
+//           NavHost
+        }
+    )
 }
