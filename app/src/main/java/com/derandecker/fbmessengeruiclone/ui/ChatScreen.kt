f37e6f0e6b6761c12d1e3fbc07dd1849016d09e9
Deran Decker <me@derandecker.com>
2023-03-05 21:10:36 -1000

    setup profile image. setup fake data. setup time.

--- app/src/main/java/com/derandecker/fbmessengeruiclone/ui/ChatScreen.kt
+++ app/src/main/java/com/derandecker/fbmessengeruiclone/ui/ChatScreen.kt
@@ -1,7 +1,7 @@
 package com.derandecker.fbmessengeruiclone.ui
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
@@ -9,14 +9,24 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.derandecker.fbmessengeruiclone.R
+import com.derandecker.fbmessengeruiclone.chatListItems
+import com.derandecker.fbmessengeruiclone.recentContactsList
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("MM-dd-yyyy hh:mm a")
 
 @Preview
 @Composable
@@ -56,58 +66,27 @@ fun ChatList() {
     LazyColumn {
         // add key when using real data
         items(items = chatListItems) {
-            ChatItem(it)
+            ChatItem(it.name, it.message, it.time, it.profilePicture)
         }
     }
 }
 
 @Composable
-fun ChatItem(name: String) {
+fun ChatItem(name: String, message: String, time: LocalDateTime, @DrawableRes profilePicture: Int) {
     Row(modifier = Modifier.padding(bottom = 8.dp)) {
-        Box(
-            modifier = Modifier
-                .background(Color.Blue)
-                .size(40.dp)
+        Image(
+            modifier = Modifier.size(40.dp)
+                .clip(CircleShape),
+            painter = painterResource(id = profilePicture),
+            contentDescription = stringResource(R.string.profile_picture)
         )
-        Column(modifier = Modifier.padding(start = 4.dp)) {
+        Column(modifier = Modifier.padding(start = 8.dp)) {
             Text(text = name)
-            Text(text = "Sample recent chat text" + " * 9:99 pm")
+            Text(text = "$message ${time.format(formatter)}")
         }
     }
 }
 
 @Composable
 fun RecentContactItem() {
-
 }
-
-val recentContactsList = listOf("Jane Doe", "John Doe", "Jim Doe")
-val chatListItems = listOf(
-    "Kim Doe",
-    "Amy Doe",
-    "James Doe",
-    "Kim Doe",
-    "Amy Doe",
-    "James Doe",
-    "Kim Doe",
-    "Amy Doe",
-    "James Doe",
-    "Kim Doe",
-    "Amy Doe",
-    "James Doe",
-    "Kim Doe",
-    "Amy Doe",
-    "James Doe",
-    "Kim Doe",
-    "Amy Doe",
-    "James Doe",
-    "Kim Doe",
-    "Amy Doe",
-    "James Doe",
-    "Kim Doe",
-    "Amy Doe",
-    "James Doe",
-    "Kim Doe",
-    "Amy Doe",
-    "James Doe",
-)
